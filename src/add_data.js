8e1ed89faa59b1576688b359dd87e3a766fadcab
Mayank Gupta <mayank.pkgupta@gmail.com>
2023-11-29 22:45:57 -0500

    add data script

---
+++ src/add_data.js
@@ -0,0 +1,16 @@
+// import projects from "./data.json" assert { type: "json" }
+// import { collection, addDoc } from "firebase/firestore"
+// import { db } from "../firebase/clientApp.js"
+
+// const addData = async () => {
+//   projects.projects.forEach(async (el) => {
+//     const d = await JSON.parse(JSON.stringify(el))
+
+//     const docRef = await addDoc(collection(db, "projects_updated"), d)
+//     console.log("Document written with ID: ", docRef.id)
+//   })
+// }
+
+// addData()
+
+// export default addData
