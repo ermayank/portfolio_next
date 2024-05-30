9fa287aaaba9ddbcdfb10a473110b16cda4cc886
Mayank Gupta <mayank.pkgupta@gmail.com>
2024-05-30 17:41:25 -0400

    Blogs page structure

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,9 +1,53 @@
+import Footer from "@/components/Footer"
+import { client } from "../../../sanity/lib/client"
+
+async function getBlogs() {
+  const query = `
+  *[_type == "post"] | order(_createdAt desc) {
+    title,
+      "currentSlug":slug.current,
+      "imageUrl":mainImage.asset->url,
+      "creationDate":_createdAt
+  }`
+
+  const data = await client.fetch(query)
+  return data
+}
+
 const Blog = async () => {
-    return (
-        <>
-        <div>hello</div>
-        </>
-    )
+  const blogs = await getBlogs()
+  return (
+    <>
+      <div className="page-container">
+        <div className="container">
+          <div className="section-title">
+            <h2>Latest Blogs</h2>
+            {/* <p>{router.query.name}</p> */}
+          </div>
+          <div className="row">
+            {blogs.map((post) => (
+              <div className="col-lg-4 col-md-6 col-sm-6 mt-5">
+                <div className="post-card">
+                  <img
+                    src={post.imageUrl}
+                    alt={post.title}
+                    className="post-image"
+                  />
+                  <div className="post-content">
+                    <h3 className="post-title">{post.title}</h3>
+                    {/* <p className="post-text">{post.text}</p> */}
+                    <p className="post-date">{post.creationDate}</p>
+                    {/* <button>Read More</button> */}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <Footer></Footer>
+    </>
+  )
 }
 
 export default Blog
